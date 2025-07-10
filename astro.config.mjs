0c6bdcafc73bbb169e24a658ba36fd0a82888188
Elke Heymans <el.heymans@gmail.com>
2025-07-10 18:36:58 +0200

    Added netlify adapter and usage of getSecret()

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://elkecodes.dev",
+
   image: {
     responsiveStyles: true,
   },
+
   integrations: [svelte()],
+  adapter: netlify(),
 });
