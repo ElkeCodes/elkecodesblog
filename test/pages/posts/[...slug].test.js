a18c612617d296f87524f9af8970978e525bf4b2
Elke Heymans <el.heymans@gmail.com>
2025-08-19 06:35:26 +0200

    Upgraded astro to 5.13.2, cleaned up old code and did astro check

--- test/pages/posts/[...slug].test.js
+++ test/pages/posts/[...slug].test.js
@@ -2,7 +2,7 @@ import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import { expect, describe, it } from "vitest";
 import blogpost from "../../../src/pages/posts/[...slug].astro";
 import svelteRenderer from "@astrojs/svelte/server.js";
-import { createBlogPost, mockBlogPost } from "test/__mocks__/blogpost";
+import { createBlogPost } from "test/__mocks__/blogpost";
 
 describe("blogpost page", () => {
   it("should render", async () => {
